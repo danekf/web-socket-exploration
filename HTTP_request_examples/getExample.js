ada91e25d52159c4da88da07b5e5a5bbfbd40c92
Danek <danek.fill@outlook.com>
2022-08-26 16:25:21 -0400

    Add websocket and server js file

--- HTTP_request_examples/getExample.js
+++ HTTP_request_examples/getExample.js
@@ -3,6 +3,7 @@ const axios = require('axios');
 
 const getThisRepo = () =>{
   axios
+  // .get is the code to tell axios what to ask the server it is connected to for. Here we are just asking for a web page. In this case, the main page for the repository.
     .get('https://github.com/danekf/web-socket-exploration#What%20is%20a%20web%20socket?')
     .then(res => {
       console.log(`statusCode: ${res.status}`);
